7726aace8028302dec0fc627c358fd10faecb531
S.Y <70691930+NewGoldSmith@users.noreply.github.com>
2024-06-18 20:42:54 +0900

    Add files via upload

---
+++ SubProcess/MemoryLoan.h
@@ -0,0 +1,245 @@
+﻿/**
+ * @file MemoryLoan.h
+ * @brief テンプレートメモリプールクラスの実装
+ * @author Gold Smith<br>
+ * @date 2022-2024<br>
+ * @copyright SPDX-License-Identifier: MIT<br>
+ * Released under the MIT license<br>
+ * https: //opensource.org/licenses/mit-license.php<br>
+ * このファイル内のすべてのコードは、特に明記されていない限り、MITライセンスに従います。
+ */
+
+#pragma once
+ /// @attention このクラスはWindows専用です。<br>
+ /// 異常が検出された場合、必ず例外が発生します。
+
+// ********使用条件を設定**************************
+
+/// @def ML_USING_CRITICAL_SECTION
+/// @brief クリティカルセクションを使用する場合
+#define ML_USING_CRITICAL_SECTION
+
+/// @def ML_CONFIRM_RANGE
+/// @brief 範囲外確認を行う場合。
+#define ML_CONFIRM_RANGE
+
+/// @def ML_USING_DEBUG_OUT
+/// @brief デバッグ出力をする場合。
+#define ML_USING_DEBUG_OUT
+
+/// @def ML_USING_STD_ERROR
+/// @brief std::cerr出力をする場合。
+#define ML_USING_STD_ERROR
+
+// **********条件設定終わり************************
+
+#include <memory>
+#include <string>
+#include <exception>
+#include <iostream>
+#include <sstream>
+
+#ifdef ML_USING_DEBUG_OUT
+#include <debugapi.h>
+#endif // ML_USING_DEBUG_OUT
+#if defined(ML_CONFIRM_RANGE) || defined(ML_USING_DEBUG_OUT) || defined(ML_USING_STD_ERROR)
+#include <algorithm>
+#endif
+#ifdef ML_USING_CRITICAL_SECTION
+#include <synchapi.h>
+#endif // ML_USING_CRITICAL_SECTION
+
+template <typename T>
+class MemoryLoan {
+public:
+	MemoryLoan() = delete;
+	// @attention sizeInは2のべき乗でなくてはなりません。
+	MemoryLoan(T *const pBufIn, size_t sizeIn)
+		:ppBuf(nullptr)
+		, front(0)
+		, end(0)
+#if defined(ML_CONFIRM_RANGE) || defined(ML_USING_DEBUG_OUT) || defined(ML_USING_STD_ERROR)
+		, max_using(0)
+#endif
+		, mask(sizeIn - 1)
+#ifdef ML_USING_CRITICAL_SECTION
+		, cs{}
+#endif // ML_USING_CRITICAL_SECTION
+
+	{
+		if ((sizeIn & (sizeIn - 1)) != 0) {
+			std::string estr("The number of units for MemoryLoan must be specified as a power of 2.\r\n");
+#ifdef ML_USING_DEBUG_OUT
+			::OutputDebugStringA(estr.c_str());
+#endif// ML_USING_DEBUG_OUT
+#ifdef ML_USING_STD_ERROR
+			std::cerr << estr;
+#endif // ML_USING_STD_ERROR
+			throw std::invalid_argument(estr);
+		}
+#ifdef ML_USING_CRITICAL_SECTION
+		(void)::InitializeCriticalSection(&cs);
+#endif // ML_USING_CRITICAL_SECTION
+		ppBuf = new T * [sizeIn];
+		for (size_t i(0); i < sizeIn; ++i) {
+			ppBuf[i] = &pBufIn[i];
+		}
+	}
+	MemoryLoan(const MemoryLoan &) = delete;
+	MemoryLoan(const MemoryLoan &&)noexcept = delete;
+	MemoryLoan &operator ()(const MemoryLoan &) = delete;
+	MemoryLoan &operator =(const MemoryLoan &) = delete;
+	MemoryLoan &operator ()(MemoryLoan &&) = delete;
+	MemoryLoan &operator =(MemoryLoan &&) = delete;
+	~MemoryLoan() {
+#ifdef ML_USING_DEBUG_OUT
+		std::stringstream ss;
+		ss << "MemoryLoan is destructing."
+			<< " DebugMessage:" << "\"" << strDebug << "\""
+			<< " TypeName:" << "\"" << typeid(T).name() << "\""
+			<< " BytesPerUnit:" << sizeof(T) << "bytes"
+			<< " TotalNumberOfLoans:" << std::to_string(end)
+			<< " TotalNumberOfReturns:" << std::to_string(front)
+			<< " NumberOfUnreturned:" << std::to_string((int)(end - front))
+			<< " NumberOfUnits:" << std::to_string(mask + 1)
+			<< " MaximumNumberOfLoans:" << std::to_string(max_using)
+			<< "\r\n";
+		::OutputDebugStringA(ss.str().c_str());
+#endif // ML_USING_DEBUG_OUT
+
+#ifdef ML_USING_CRITICAL_SECTION
+		::DeleteCriticalSection(&cs);
+#endif // ML_USING_CRITICAL_SECTION
+		delete[] ppBuf;
+	}
+
+	// @attention sizeInは2のべき乗でなくてはなりません。
+	void ReInitialized(T *const pBufIn, size_t sizeIn) {
+#ifdef ML_USING_DEBUG_OUT
+		std::stringstream ss;
+		ss << "MemoryLoan reinitialized."
+			<< " DebugMessage:" << "\"" << strDebug << "\""
+			<< " TypeName:" << "\"" << typeid(T).name() << "\""
+			<< " BytesPerUnit:" << sizeof(T) << "bytes"
+			<< " TotalNumberOfLoans:" << std::to_string(end)
+			<< " TotalNumberOfReturns:" << std::to_string(front)
+			<< " NumberOfUnreturned:" << std::to_string((int)(end - front))
+			<< " NumberOfUnits:" << std::to_string(mask + 1)
+			<< " MaximumNumberOfLoans:" << std::to_string(max_using)
+			<< "\r\n";
+		::OutputDebugStringA(ss.str().c_str());
+#endif // ML_USING_DEBUG_OUT
+		delete[] ppBuf;
+		front = 0;
+		end = 0;
+		mask = sizeIn - 1;
+		if ((sizeIn & (sizeIn - 1)) != 0) {
+			std::string estr("The number of units for MemoryLoan must be specified as a power of 2.\r\n");
+#ifdef ML_USING_DEBUG_OUT
+			::OutputDebugStringA(estr.c_str());
+#endif// ML_USING_DEBUG_OUT
+#ifdef ML_USING_STD_ERROR
+			std::cerr << estr;
+#endif // ML_USING_STD_ERROR
+			throw std::invalid_argument(estr);
+		}
+		ppBuf = new T * [sizeIn];
+		for (size_t i(0); i < sizeIn; ++i) {
+			ppBuf[i] = &pBufIn[i];
+		}
+	}
+
+	inline T *Lend() {
+#ifdef ML_USING_CRITICAL_SECTION
+		std::unique_ptr< ::CRITICAL_SECTION, decltype(::LeaveCriticalSection) *> qcs
+			= { [&]() {::EnterCriticalSection(&cs); return &cs; }(),::LeaveCriticalSection };
+#endif // ML_USING_CRITICAL_SECTION
+#ifdef ML_CONFIRM_RANGE
+		if ((front + mask) < end) {
+			std::stringstream ss;
+			ss << __FILE__ << "(" << __LINE__ << "):"
+				<< "Loans will soon surpass units."
+				<< " DebugMessage:" << "\"" << strDebug << "\""
+				<< " TypeName:" << "\"" << typeid(T).name() << "\""
+				<< " BytesPerUnit:" << sizeof(T) << "bytes"
+				<< " TotalNumberOfLoans:" << std::to_string(end)
+				<< " TotalNumberOfReturns:" << std::to_string(front)
+				<< " NumberOfUnreturned:" << std::to_string((int)(end - front))
+				<< " NumberOfUnits:" << std::to_string(mask + 1)
+				<< " MaximumNumberOfLoans:" << std::to_string(max_using)
+				<< "\r\n";
+#ifdef ML_USING_STD_ERROR
+			std::cerr << ss.str();
+#endif // ML_USING_STD_ERROR
+#ifdef ML_USING_DEBUG_OUT
+			::OutputDebugStringA(ss.str().c_str());
+#endif// ML_USING_DEBUG_OUT
+			throw std::out_of_range(ss.str().c_str()); // 例外送出
+		}
+#endif // ML_CONFIRM_RANGE
+		T **ppT = &ppBuf[end & mask];
+		++end;
+#if defined(ML_CONFIRM_RANGE) || defined(ML_USING_DEBUG_OUT) || defined(ML_USING_STD_ERROR)
+		max_using = std::max<size_t>(end - front, max_using);
+#endif
+		return *ppT;
+	}
+
+	inline void Return(T *const pT) {
+#ifdef ML_USING_CRITICAL_SECTION
+		std::unique_ptr< ::CRITICAL_SECTION, decltype(::LeaveCriticalSection) *> qcs
+			= { [&]() {::EnterCriticalSection(&cs); return &cs; }(),::LeaveCriticalSection };
+#endif // ML_USING_CRITICAL_SECTION
+#ifdef ML_CONFIRM_RANGE
+		if ((front + 1) > end) {
+			std::stringstream ss;
+			ss << __FILE__ << "(" << __LINE__ << "):"
+				<< " Returns exceed loans."
+				<< " DebugMessage:" << "\"" << strDebug << "\""
+				<< " TypeName:" << "\"" << typeid(T).name() << "\""
+				<< " BytesPerUnit:" << sizeof(T) << "bytes"
+				<< " TotalNumberOfLoans:" << std::to_string(end)
+				<< " TotalNumberOfReturns:" << std::to_string(front)
+				<< " NumberOfUnreturned:" << std::to_string((int)(end - front))
+				<< " NumberOfUnits:" << std::to_string(mask + 1)
+				<< " MaximumNumberOfLoans:" << std::to_string(max_using)
+				<< "\r\n";
+#ifdef ML_USING_STD_ERROR
+			std::cerr << ss.str();
+#endif // ML_USING_STD_ERROR
+#ifdef ML_USING_DEBUG_OUT
+			::OutputDebugStringA(ss.str().c_str());
+#endif// ML_USING_DEBUG_OUT
+			throw std::out_of_range(ss.str().c_str()); // 例外送出
+		}
+#endif // ML_CONFIRM_RANGE
+		ppBuf[front & mask] = pT;
+		++front;
+	}
+
+	void DebugString(const std::string &str) {
+#if defined(ML_CONFIRM_RANGE) ||defined(ML_USING_DEBUG_OUT) || defined(ML_USING_STD_ERROR)
+		strDebug = str;
+#endif
+	}
+
+protected:
+	T **ppBuf;
+	size_t front;
+	size_t end;
+	size_t mask;
+#if defined(ML_CONFIRM_RANGE) ||defined(ML_USING_DEBUG_OUT) || defined(ML_USING_STD_ERROR)
+	size_t max_using;
+	std::string strDebug;
+#endif
+
+#ifdef ML_USING_CRITICAL_SECTION
+	::CRITICAL_SECTION cs;
+#endif // ML_USING_CRITICAL_SECTION
+
+};
+
+#undef ML_USING_CRITICAL_SECTION
+#undef ML_CONFIRM_RANGE
+#undef ML_USING_DEBUG_OUT
+#undef ML_USING_STD_ERROR
---
+++ SubProcess/SubProcess.h
@@ -0,0 +1,91 @@
+﻿/**
+ * @file SubProcess.h
+ * @brief SubProcess作成クラス宣言
+ * SPDX-License-Identifier: MIT<br>
+ * @date 2024<br>
+ * @author Gold Smith
+ */
+#pragma once
+#define STRINGIZE_(x) #x
+#define STRINGIZE(x) STRINGIZE_(x)
+
+#include <Windows.h>
+#include <string>
+#include <iomanip>
+#include <iosfwd>
+#include <xiosbase>
+
+#include "../Debug_fnc/debug_fnc.h"
+#include "./MemoryLoan.h"
+#pragma comment(lib,  "../Debug_fnc/" STRINGIZE($CONFIGURATION) "/Debug_fnc-" STRINGIZE($CONFIGURATION) ".lib")
+
+class  SubProcess {
+	static constexpr int BUFFER_SIZE = 0x400;
+	static constexpr int PIPE_BUFFER_SCALE = 1;
+	static constexpr int NUM_OVERLAPPED = 0x8;
+	static constexpr int DEFAULT_TIMEOUT = 1000;
+	static constexpr int CONTINUOUS_TIMEOUT = 1;
+	struct OVERLAPPED_CUSTOM;
+public:
+	SubProcess()noexcept;
+	~SubProcess() {};
+	void ResetFlag()noexcept;
+	DWORD SetTimeOut(DWORD uiTime)noexcept;
+	bool IsTimeOut()const noexcept { return __numErr == WAIT_TIMEOUT; };
+	bool Popen(const std::string &strCommand);
+	bool Pclose();
+	DWORD GetExitCodeSubProcess();
+	bool TerminateProcess(DWORD dw);
+	bool WaitForTermination(DWORD time);
+	DWORD GetLastError()const noexcept;
+	explicit operator bool() const noexcept { return !__numErr; }
+	bool IsActive();
+	bool IsReadable();
+	bool SetUseStdErr(bool is_use);
+	SubProcess &operator<<(const std::string &str);
+	SubProcess &operator<< (std::istream &is);
+	SubProcess &operator<<(std::ostream &(*const manipulator)(std::ostream &));
+	SubProcess &operator<<(std::ios_base &(*const manipulator)(std::ios_base &));
+	SubProcess &SleepEx(DWORD num);
+	SubProcess &Flush();
+	SubProcess &ClearBuffer();
+	SubProcess &operator>>(std::string &str);
+	SubProcess &operator>>(std::ostream &os);
+	friend std::ostream &operator<<(std::ostream &os, SubProcess &sp);
+	SubProcess &Await(DWORD numAwaitTime);
+	SubProcess &CErr();
+private:
+	bool __ClosePipes();
+	bool __FlushWrite();
+	bool __FlushRead();
+	bool __CancelIo();
+	bool __WriteToCli(const std::string &str);
+	bool __ReadFromCli();
+	bool __ReadFromCliErr();
+	bool __StrongReadFromCli(DWORD timeout= DEFAULT_TIMEOUT);
+	std::wstring __AtoW(const std::string &str)const;
+	std::string __GetParentPathA();
+	std::wstring __CreateNamedPipeStringW();
+	struct OVERLAPPED_CUSTOM {
+		::OVERLAPPED __ol{};
+		SubProcess *self{};
+		char __buffer[BUFFER_SIZE]{};
+	};
+
+	OVERLAPPED_CUSTOM __OLArr[NUM_OVERLAPPED];
+	MemoryLoan< OVERLAPPED_CUSTOM> __mlOL;
+	bool __bfIsUseStdErr{};
+	bool __bfIsErrOut{};
+	::HANDLE __hSevR{}, __hSevW{}, __hCliR{}, __hCliW{}, __hErrW{}, __hErrR{};
+	DWORD __numErr{ 0 };
+	DWORD __numAwait{ 0 };
+	DWORD __numTimeOut{ DEFAULT_TIMEOUT };
+	DWORD __numContinuousTimeOut{ CONTINUOUS_TIMEOUT };
+	::PROCESS_INFORMATION __PI{};
+	::LPOVERLAPPED_COMPLETION_ROUTINE const __pfReadFromChildCompleted;
+	::LPOVERLAPPED_COMPLETION_ROUTINE const __pfWriteToChildCompleted;
+	std::ostringstream __ToChildBuf;
+	std::stringstream __FromChildBuf;
+	std::stringstream __FromChildBufErr;
+};
+
