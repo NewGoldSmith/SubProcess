078d198e27e043313f3c02868a7a29d93ead7cee
GoldSmith <70691930+NewGoldSmith@users.noreply.github.com>
2024-06-20 20:54:12 +0900

    Delete TestProject/test.bat

--- TestProject/test.bat
+++
@@ -1,2 +0,0 @@
-@ssh usera@192.168.100.205 "@"D:"&@"cd /Users/usera/shogi/engine/suisho5"&"chcp 65001"&"Suisho5-ZEN2.exe" 
-rem ssh 192.168.100.205 "@D:&@cd /Users/usera/shogi/engine/suisho5&@Suisho5-ZEN2.exe"
