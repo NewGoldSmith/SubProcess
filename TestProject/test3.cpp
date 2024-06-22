1c5e17a34d43a93326d18d46505e5890471f97f7
GoldSmith <70691930+NewGoldSmith@users.noreply.github.com>
2024-06-23 04:09:32 +0900

    Add files via upload

---
+++ TestProject/test3.cpp
@@ -0,0 +1,39 @@
+﻿// test3.cpp
+#include "test2.h"
+using namespace std;
+
+int main() {
+	{
+		string str;										// １
+		SubProcess sp;
+		if (!sp.Popen(R"(cmd.exe /c RemoteCmd.bat)"))
+			return 1;
+
+		sp.SleepEx(2000);								// ２
+		
+		for (; sp.IsActive();) {					// ３
+			if (sp.IsReadable()) {
+				if (!(sp >> cout))
+					return 1;
+				else
+					sp.SleepEx(10);
+					continue;
+
+			} else {
+				if (!(sp << cin))
+					return 1;
+				sp.SleepEx(10);
+			}
+		}
+
+		if (!(sp.WaitForTermination(INFINITE)))// ４
+			return 1;
+
+		DWORD dw = sp.GetExitCodeSubProcess();	// ５
+		cout << "\nExit code is " << dw << endl;
+
+		sp.Pclose();									// ６
+	}
+	_CrtDumpMemoryLeaks();
+
+}
