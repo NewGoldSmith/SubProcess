e9d5fb866e779e66124d3b92aec5735df53d2d62
GoldSmith <70691930+NewGoldSmith@users.noreply.github.com>
2024-07-02 22:59:34 +0900

    Add files via upload

---
+++ TestProject/testIOCP.cpp
@@ -0,0 +1,22 @@
+﻿/**
+ * @file testIOCP.cpp
+ * @brief IOCPテスト実装
+ * SPDX-License-Identifier: MIT<br>
+ * @date 2024<br>
+ * @author Gold Smith
+ */
+#include "testIOCP.h"
+using namespace std;
+int main() {
+	{
+		sampleIOCP s;// 1
+		if (!(s << "Hello, World!"))// 2
+			return 1;
+		string str;
+		if (!(s >> str))// 3
+			return 1;
+		s.OrCout.Push("Main:\"" + str + "\"");// 4
+	}
+	_CrtDumpMemoryLeaks();
+	return 0;
+}
---
+++ TestProject/testIOCP.h
@@ -0,0 +1,14 @@
+﻿/**
+ * @file testIOCP.h
+ * @brief IOCPテスト宣言
+ * SPDX-License-Identifier: MIT<br>
+ * @date 2024<br>
+ * @author Gold Smith
+ */
+#include <crtdbg.h>
+#include "../CommonLib/defSTRINGIZE.h"
+#include "../CommonLib/sampleIOCP.h"
+
+#pragma comment(lib,  "../CommonLib/" STRINGIZE($CONFIGURATION) "/CommonLib-" STRINGIZE($CONFIGURATION) ".lib")
+
+#pragma once
