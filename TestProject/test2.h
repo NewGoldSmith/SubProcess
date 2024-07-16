c16949d95f04401911dd92c306239f8a911588e6
GoldSmith <70691930+NewGoldSmith@users.noreply.github.com>
2024-07-16 21:05:52 +0900

    Add files via upload

--- TestProject/test2.h
+++ TestProject/test2.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <crtdbg.h>
-#include "../SubProcess/SubProcess.h"
+#include "../SubProcess/SubProcess2.h"
 #include "../CommonLib/defSTRINGIZE.h"
 
 #pragma comment(lib,  "../SubProcess/" STRINGIZE($CONFIGURATION) "/SubProcess-" STRINGIZE($CONFIGURATION) ".lib")
-#pragma comment(linker,"/STACK:1000000000")
+//#pragma comment(linker,"/STACK:10000000")
